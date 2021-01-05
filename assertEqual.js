85e83365ac79068680545b546528016b9135c5e6
Elliott Thomlison <elliott.thomlison@gmail.com>
2021-01-05 02:26:08 +0000

    Here we go

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
-const assertEqual = function(a, b) {
-  return a + b ;
-};
-
-console.assert(assertEqual(2, 2) === 4);
-console.assert(assertEqual(1, 10) === 50);
-console.assert(assertEqual(25, 25) === 50);
-console.assert(assertEqual(1, 100) === 5);
-
+let assertEqual = (actual, expected) => {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: "${actual}" !== "${expected}"`);
+    }
+  };
+  module.exports = assertEqual;
+  assertEqual;
