66e9fe6fb76f50b7f80e0e6f3c80283b16598b2a
Elliott Thomlison <elliott.thomlison@gmail.com>
2021-01-05 02:45:58 +0000

    Here we go

---
+++ head.js
@@ -0,0 +1,17 @@
+let assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+module.exports = assertEqual;
+
+function head (arr) {
+  return arr[0];
+}
+
+assertEqual(head([5,6,7], 5));
+assertEqual(head(["Hello", "Lighthouse", "Labs"], "Hello"));
+
+
