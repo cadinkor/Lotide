d4b0007cb20486cb68dfd4754e451ed75cfa469f
cadinkor <elliott.thomlison@gmail.com>
2021-02-03 17:50:59 +0000

    Reviewing

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,12 @@
-let assertEqual = (actual, expected) => {
-  if (actual === expected) {
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
     console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: "${actual}" !== "${expected}"`);
+  }else{
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-module.exports = assertEqual;
+}
 
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(100,200)
+assertEqual("Sam", "Sam")
--- head.js
+++ head.js
@@ -1,15 +1,14 @@
-let assertEqual = (actual, expected) => {
-  if (actual === expected) {
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
     console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: "${actual}" !== "${expected}"`);
+  }else{
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-module.exports = assertEqual;
-
-function head (arr) {
-  return arr[0];
 }
 
-assertEqual(head([5,6,7]),5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
+function head(arr){
+  return arr[0] 
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
